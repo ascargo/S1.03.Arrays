bafbfa379071933cbf5d87bd1b83d71396b5f55d
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-04 12:55:56 +0200

    updated file w/out prints

--- Nivel 1/N1E4.php
+++ Nivel 1/N1E4.php
@@ -5,13 +5,4 @@ $student = array(
     "email"=>"axiercarrasco@gmail.com", 
     "favourite meal"=>"bocata de tortilla de chorizo");
 
-    print_r ($student);
-
-    var_dump($student);
-
-    echo "Name: " . $student['Name'] . "\n";
-echo "Age: " . $student['Age'] . "\n";
-echo "Email: " . $student['email'] . "\n";
-echo "Favourite meal: " . $student['favourite meal'] . "\n";
-
 ?>

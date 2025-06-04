e9cd6c9110aedba2f79f6672ba4ff600b0c25ce7
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-04 11:06:43 +0200

    change do while for Foreach

--- Nivel 1/N1E1.php
+++ Nivel 1/N1E1.php
@@ -1,9 +1,7 @@
 <?php
 $integers = array(1, 2, 3, 4, 5);
 
-$i=0;
-do {
-    echo $integers[$i] . "\n";
-    $i++;
-} while ($i < count($integers));
+foreach ($integers as $value) {
+    echo "$value </n>";
+}
 ?>
